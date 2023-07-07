7a85151bcd01a04544e065e1b598fb4bfa6f30ba
CallMeKalmi <138873292+CallMeKalmi@users.noreply.github.com>
2023-07-07 23:09:58 +0200

    Objective

--- Assets/KalmiScripts/Objective.cs
+++ Assets/KalmiScripts/Objective.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Objective : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] int _hp = 2;
     // Update is called once per frame
     void Update()
     {
-        
+        if (_hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bug"))
+        {
+            Destroy(collision.gameObject);
+            _hp--;
+        }
     }
 }
