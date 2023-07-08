20f434b2f4a29d683e22dcb00604a02fb6210ce4
CallMeKalmi <138873292+CallMeKalmi@users.noreply.github.com>
2023-07-08 15:34:46 +0200

    Full UI

--- Assets/KalmiScripts/MainLevelUI.cs
+++ Assets/KalmiScripts/MainLevelUI.cs
@@ -22,33 +22,62 @@ public class MainLevelUI : MonoBehaviour
     [SerializeField] Button _attackButton2;
     [SerializeField] Button _attackButton3;
 
-    [SerializeField] GameObject _objective1;
-    [SerializeField] GameObject _objective2;
-    [SerializeField] GameObject _objective3;
+    //[SerializeField] GameObject _objective1;
+    //[SerializeField] GameObject _objective2;
+    //[SerializeField] GameObject _objective3;
 
     int _firstBugNumber = 5;
     int _secondBugNumber = 3;
     int _thirdBugNumber = 4;
     int _fourthBugNumber = 6;
 
-    Vector2 _spawnLocation;
+    Vector3 _spawnLocation;
     [SerializeField] GameObject _prefabObject;
-    //[SerializeField] Transform _spawnPoit1;
-    //[SerializeField] Transform _spawnPoit2;
-    //[SerializeField] Transform _spawnPoit3;
+    Transform _spawnPoit1;
+    Transform _spawnPoit2;
+    Transform _spawnPoit3;
     // Start is called before the first frame update
     void Start()
+    {
+        DeactivateBottomButtons();
+        
+
+        _spawnPoit1 = _attackButton1.GetComponent<RectTransform>();
+        _spawnPoit2 = _attackButton2.GetComponent<RectTransform>();
+        _spawnPoit3 = _attackButton3.GetComponent<RectTransform>();
+    }
+
+    private void DeactivateBottomButtons()
     {
         _button1.interactable = false;
         _button2.interactable = false;
         _button3.interactable = false;
         _button4.interactable = false;
     }
+    private void ActivateBottomButtons()
+    {
+        _button1.interactable = true;
+        _button2.interactable = true;
+        _button3.interactable = true;
+        _button4.interactable = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
         TextUpdate();
+        CheckIfPathsAreAvalible();
+    }
+
+    private void CheckIfPathsAreAvalible()
+    {
+        if (!_attackButton1.IsInteractable())
+        {
+            if (_spawnLocation == _spawnPoit1.position)
+            {
+                DeactivateBottomButtons();
+            }
+        }
     }
 
     private void TextUpdate()
@@ -61,35 +90,28 @@ public class MainLevelUI : MonoBehaviour
 
     public void FirstAttackButtonPress()
     {
-        _button1.interactable = true;
-        _button2.interactable = true;
-        _button3.interactable = true;
-        _button4.interactable = true;
-
-        RectTransform uiElementRect = _attackButton1.GetComponent<RectTransform>();
-        _spawnLocation = uiElementRect.position;
+        ActivateBottomButtons();
+        //RectTransform uiElementRect = _attackButton1.GetComponent<RectTransform>();
+        _spawnLocation = _spawnPoit1.position;
     }
+
+    
+
     public void SecondAttackButtonPress()
     {
-        _button1.interactable = true;
-        _button2.interactable = true;
-        _button3.interactable = true;
-        _button4.interactable = true;
+        ActivateBottomButtons();
 
-        RectTransform uiElementRect = _attackButton2.GetComponent<RectTransform>();
-        _spawnLocation = uiElementRect.position;
+        //RectTransform uiElementRect = _attackButton2.GetComponent<RectTransform>();
+        _spawnLocation = _spawnPoit2.position;
     }
     public void ThirdAttackButtonPress()
     {
-        _button1.interactable = true;
-        _button2.interactable = true;
-        _button3.interactable = true;
-        _button4.interactable = true;
+        ActivateBottomButtons();
 
-        RectTransform uiElementRect = _attackButton3.GetComponent<RectTransform>();
+        //RectTransform uiElementRect = _attackButton3.GetComponent<RectTransform>();
         //Vector3 uiElementScreenPos = RectTransformUtility.WorldToScreenPoint(Camera.main, uiElementRect.position);
         //_spawnLocation = Camera.main.ScreenToWorldPoint(uiElementScreenPos);
-        _spawnLocation = uiElementRect.position;
+        _spawnLocation = _spawnPoit3.position;
     }
 
     public void FirstBugSpawn()
--- Assets/KalmiScripts/Objective.cs
+++ Assets/KalmiScripts/Objective.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.UIElements;
 using static UnityEngine.GraphicsBuffer;
 
 public class Objective : MonoBehaviour
 {
     [SerializeField] int _hp = 2;
+    [SerializeField] Button _button;
     // Update is called once per frame
     void Update()
     {
         if (_hp <= 0)
         {
             Destroy(gameObject);
+            _button.interactable = false;
         }
     }
 
