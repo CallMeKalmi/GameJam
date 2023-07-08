9c614bf204e0cd701f6a7c8a354939dad064113e
CallMeKalmi <138873292+CallMeKalmi@users.noreply.github.com>
2023-07-08 17:24:27 +0200

    Full rose tower

--- Assets/KalmiScripts/RoseTower.cs
+++ Assets/KalmiScripts/RoseTower.cs
@@ -1,18 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class RoseTower : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    [SerializeField] GameObject _bullet;
+    //float speed = 5f;
+    float _time = 0;
+    float _fireRate = 2f;
+    public Boolean TimerStart;
+    public float Timer;
+    public Boolean Active = true;
+
+    private void Update()
     {
-        
+        if (Active == true)
+        {
+            
+            //RotateTowards();
+            Shoot();
+
+        }
+        UpdateTimer();
+
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateTimer()
+    {
+        if (TimerStart == true)
+        {
+            Timer = Timer + Time.deltaTime;
+            if (Timer > 2)
+            {
+                Timer = 0;
+                TimerStart = false;
+                Active = true;
+
+            }
+        }
+
+    }
+
+    private void Shoot()
     {
         
+        _time += Time.deltaTime;
+        if (_time >= _fireRate)
+        {
+            _time = 0;
+            Instantiate(_bullet, transform.position, transform.rotation);
+
+        }
+
+
     }
+
+    
+
+    void Deactivate()
+    {
+        Active = false;
+        TimerStart = true;
+    }
+
+    
+
+    
 }
